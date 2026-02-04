9754797e5f3217c61aa6256837722acf0ab4ce67
mod <modularizer@gmail.com>
2026-02-04 15:32:14 -0800

    initial release

--- src/literalenum/__init__.py
+++ src/literalenum/__init__.py
@@ -4,9 +4,12 @@ from .literal_enum import LiteralEnum, LiteralEnumMeta
 from .mypy_plugin import plugin
 from .stubgen import main as lestub
 
+import typing_literalenum as core
+
 __all__ = [
     "LiteralEnum", "LiteralEnumMeta",
     "plugin",
     "compatibility_extensions",
     "lestub",
+    "core"
 ]
